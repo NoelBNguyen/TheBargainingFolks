ecdf3a00c96eab394ed639d821e40817b6b25603
Noel Nguyen <noel.b.nguyen@outlook.com>
2020-03-24 15:21:32 -0400

    YEs

---
+++ TheBargainingFolks/src/main/java/com/tbf/TheBargainingFolksApplication.java
@@ -0,0 +1,13 @@
+package com.tbf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBargainingFolksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBargainingFolksApplication.class, args);
+	}
+
+}
---
+++ TheBargainingFolks/src/test/java/com/tbf/TheBargainingFolksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBargainingFolksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
