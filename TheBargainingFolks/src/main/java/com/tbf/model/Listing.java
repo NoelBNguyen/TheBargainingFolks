114e7310a2e6f63da4eb69e5cb9d385a23f5873f
BVaihinger <bvaihinger@colgate.edu>
2020-03-29 16:28:25 -0400

    Fixed creation time for User and Listing

--- TheBargainingFolks/src/main/java/com/tbf/model/Listing.java
+++ TheBargainingFolks/src/main/java/com/tbf/model/Listing.java
@@ -1,12 +1,14 @@
 package com.tbf.model;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "ProductListings")
@@ -21,21 +23,23 @@ public class Listing {
 	private String quality;
 	private String description;
 	private Double price;
-	private LocalDateTime uploadTime;
+	
+	@CreationTimestamp
+	private Date uploadTime;
 	
 	public Listing() {
 		
 	}
 	
 	public Listing(long sellerId, String title, String category, String quality, String description, Double price,
-			LocalDateTime uploadTime) {
+			Date uploadTime) {
 		this.sellerId = sellerId;
 		this.title = title;
 		this.category = category;
 		this.quality = quality;
 		this.description = description;
 		this.price = price;
-		this.uploadTime = LocalDateTime.now();
+		this.uploadTime = new Date();
 	}
 
 	public long getId() {
@@ -94,11 +98,11 @@ public class Listing {
 		this.price = price;
 	}
 
-	public LocalDateTime getUploadTime() {
+	public Date getUploadTime() {
 		return uploadTime;
 	}
 
-	public void setUploadTime(LocalDateTime uploadTime) {
+	public void setUploadTime(Date uploadTime) {
 		this.uploadTime = uploadTime;
 	}
 
