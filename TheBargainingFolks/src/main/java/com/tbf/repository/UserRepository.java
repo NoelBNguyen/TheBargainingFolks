1b24cecb0527895e626b671ee6226243d90dee2b
BVaihinger <bvaihinger@colgate.edu>
2020-03-30 03:26:47 -0400

    Added backend login validation

--- TheBargainingFolks/src/main/java/com/tbf/repository/UserRepository.java
+++ TheBargainingFolks/src/main/java/com/tbf/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.tbf.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.tbf.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	
+	@Query("SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
+	public User checkValidLogin(@Param("email") String email, @Param("password") String pass);
 }
